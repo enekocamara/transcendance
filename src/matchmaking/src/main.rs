63f7b17b4bd7471f7541be89bfc7dbe7a798eef7
Eneko Camara <ecamara@c3r7s2.42urduliz.com>
2023-11-20 16:55:48 +0100

    3 threads Reply

--- src/matchmaking/src/main.rs
+++ src/matchmaking/src/main.rs
@@ -1,20 +1,18 @@
 #[allow(unused_imports)]
 
-use warp::Filter;
 use warp::filters::multipart;
-use warp::reply::reply;
+use warp::reply::WithStatus;
+use warp::{Filter, reply::with_status, reply::json, http::StatusCode, Reply};
+
+use serde_json::Value;
+use serde::Deserialize;
 
 use core::fmt;
-use std::convert::Infallible;
-use std::fmt::Debug;
-use std::thread::sleep;
-use std::{thread, clone};
+use std::thread;
 use std::sync::{Arc, Mutex};
-use std::error::Error;
-use std::collections::{VecDeque};
+use std::collections::VecDeque;
 use std::time::{Duration, Instant};
 
-use serde::Deserialize;
 
 use array_init::array_init;
 
@@ -24,14 +22,15 @@ use ServerSettings::ServerSettings::Settings;
 
 
 
-#[repr(u16)]
-#[derive(Deserialize,Copy,Clone)]
+#[repr(u8)]
+#[derive(Deserialize,Copy,Clone,PartialEq)]
 enum LobbyType{
     Uninitialized = 0,
     Duel = 2,
     Group = 4,
     Battalion = 10,
 }
+
 impl Default for LobbyType {
     fn default() -> Self {
         LobbyType::Uninitialized
@@ -42,7 +41,7 @@ impl fmt::Debug for Lobby {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let mut debug_struct = f.debug_struct("Lobby");
         for (index, name) in self.players.iter().enumerate() {
-            debug_struct.field(&format!("Player{}", index), name);
+            debug_struct.field(&format!("\nPlayer{}", index), name);
         }
         debug_struct.finish()
     }
@@ -58,35 +57,42 @@ struct Input {
 
 #[derive(Clone, Default)]
 struct Lobby{
-    lobby_type : u16,
+    lobby_type : LobbyType,
     players : Vec<[char;10]>
 }
 
 struct Data{
     lobbies : [Lobby; Settings::MaxLobbyNum as usize],
     free_lobbies : VecDeque<usize>,
-    num_of_active_lobbies : u16,
-    last_lobby : u16,//like a custom memory manager use the lobby_type u16 same size
-    num_of_active_players : u16
+    number_of_active_lobbies : u16,
 }
 
-impl Data{
-    fn getLastLobby(&mut self) -> Result<u16,()>{
-        let mut index : u16 = self.last_lobby;
-        loop {
-            if self.lobbies[index as usize].lobby_type == u16::MAX{
-                return Ok(self.last_lobby);
-            }
-            index = self.lobbies[index as usize].lobby_type;
-        }
-    }
-    
-}
 
 struct QueuesSys{
     duel_queue:  VecDeque<[char;10]>,
     group_queueu: VecDeque<[char;10]>,
     batallion_queueu: VecDeque<[char;10]>,
+    num_of_active_lobbies : u16,
+    num_of_active_players : u16,
+}
+#[repr(u8)]
+enum MessageType{
+    MatchmakingConnectionInfo,
+    LobbyCreationSuccessful,
+    LobbyCreationError,
+    LobbyCreationPending,
+    LobbyCreationPendingNeedMorePlayers,
+    LobbyCreationPendingNeedFreeLobby,
+}
+
+struct Message{
+    operation: MessageType,
+    message: String,
+}
+
+#[derive(serde::Serialize)]
+struct Response {
+    message: &'static str,
 }
 
 fn deserialize_char_array<'de, D>(deserializer: D) -> Result<[char; 10], D::Error>
@@ -100,23 +106,40 @@ where
     Ok(array)
 }
 
-fn join(player : Input, queues : Arc<Mutex<QueuesSys>>) -> String{
+fn join(player : Input, queues : Arc<Mutex<QueuesSys>>) -> impl Reply{
     match queues.lock() {
         Ok(mut guard) => {
             match player.lobby_type {
                 LobbyType::Duel => guard.duel_queue.push_back(player.username),
                 LobbyType::Group => guard.group_queueu.push_back(player.username),
                 LobbyType::Battalion => guard.batallion_queueu.push_back(player.username),
-                _ => return String::from("Error queue type")
+                _ => {
+                    let response = Response{
+                        message : "LobbyType invalid",
+                    };
+                    let json_response = json(&response);
+                    return warp::reply::with_status(json_response, StatusCode::INTERNAL_SERVER_ERROR);
+                }
             }
-            String::from("added to the queue")},
-        Err(_) => {String::from("Something went wrong")},
+            //String::from("added to the queue");
+            let response = Response{
+                message : "{\"ConnectKey\":\"SECRET_KEY\"}",
+            };
+            let json_response = json(&response);
+            warp::reply::with_status(json_response, StatusCode::OK)
+        },
+        Err(_) => {
+            let response = Response{
+                message : "Server Error, tray again"
+            };
+            let json_response = json(&response);
+            warp::reply::with_status(json_response, StatusCode::INTERNAL_SERVER_ERROR)
+        },
     }
 }
 
-async fn warp_function(queue :  Arc<Mutex<QueuesSys>>){
-    let hello 
-        = warp::path!("matchmaking" / "join")
+async fn warp_manager(queue :  Arc<Mutex<QueuesSys>>){
+    let hello = warp::path!("matchmaking" / "join")
         .and(warp::post())
         .and(warp::body::json())
         .map(move |data: Input| join(data, queue.clone()));
@@ -126,53 +149,33 @@ async fn warp_function(queue :  Arc<Mutex<QueuesSys>>){
         .await;
 }
 
-fn frame(data : &mut Data){
-
-}
-
-fn handle_queue(data : &mut Data, queue : &mut VecDeque<[char;10]>, lobbyType : LobbyType){
-    if data.free_lobbies.len() == 0 {return;}
-
-    let lobby_index : usize = data.free_lobbies.pop_front().unwrap();
-    for i in 0..lobbyType as usize{
-        println!("i{}",i);
-        let username = queue.pop_front().unwrap();
-        data.lobbies[lobby_index].players.push(username);
+fn frame(data : Arc<Mutex<Data>>){
+    match data.lock() {
+        Ok(mut guarded_data) => {
+            if guarded_data.number_of_active_lobbies == 0 {
+                return ;
+            }
+            let mut num_of_lobbies_framed : u16 = 0;
+            let number_of_active_lobbies = guarded_data.number_of_active_lobbies;
+            for lobby in guarded_data.lobbies.iter_mut() {
+                if lobby.lobby_type != LobbyType::Uninitialized{
+                    num_of_lobbies_framed += 1;
+                }
+                if num_of_lobbies_framed == number_of_active_lobbies{
+                    break ;
+                }
+            }
+        },
+        Err(_) => {}
     }
-    data.lobbies[lobby_index].lobby_type = lobbyType as u16;
-    println!("{:?}", data.lobbies[lobby_index]);
 }
 
-fn check_queue(queues :  Arc<Mutex<QueuesSys>>){
-    let mut data : Data = Data{
-        lobbies : array_init(|_| Lobby::default()),
-        free_lobbies : (0..Settings::MaxLobbyNum as usize).collect(),
-        num_of_active_lobbies : 0,
-        num_of_active_players : 0,
-        last_lobby : 0,
-    };
+fn logic_manager(data :  Arc<Mutex<Data>>){
     let tic_rate = Settings::TicRate as u64;
     let tic_time = Duration::from_millis(1000 / tic_rate);
     let mut last_frame = Instant::now();
-    let mut tic_count = 0;
     loop {
-        if tic_count >= tic_rate{
-            match queues.lock() {
-                Ok(mut guard) => {
-                    println!("Duel:{},Group{},Battalion{}",guard.duel_queue.len(),guard.group_queueu.len(),guard.batallion_queueu.len());
-                    if guard.duel_queue.len() >= 2 && data.num_of_active_players + 2 < Settings::MaxPlayerNum as u16{
-                        handle_queue(&mut data, &mut guard.duel_queue, LobbyType::Duel);}
-                    else if guard.group_queueu.len() >= 4 && data.num_of_active_players + 4 < Settings::MaxPlayerNum as u16{
-                        handle_queue(&mut data, &mut guard.group_queueu, LobbyType::Group);}
-                    else if guard.batallion_queueu.len() >= 10 && data.num_of_active_players + 10 < Settings::MaxPlayerNum as u16{
-                        handle_queue(&mut data, &mut guard.batallion_queueu, LobbyType::Battalion);}
-                },
-                Err(_)=>{}
-            }
-            tic_count = 0;
-        }
-        tic_count += 1;
-        frame(&mut data);
+        frame(data.clone());
         let elapsed_frame = last_frame.elapsed();
         if elapsed_frame < tic_time {
             std::thread::sleep(tic_time - elapsed_frame);
@@ -183,19 +186,32 @@ fn check_queue(queues :  Arc<Mutex<QueuesSys>>){
     }
 }
 
-fn connections_function(){
+fn queues_manager(data : Arc<Mutex<Data>>, queues : Arc<Mutex<QueuesSys>>){
     let tic_time = Duration::from_millis(1000);
     let mut last = Instant::now();
     loop {
         match queues.lock() {
-            Ok(mut guard) => {
-                println!("Duel:{},Group{},Battalion{}",guard.duel_queue.len(),guard.group_queueu.len(),guard.batallion_queueu.len());
-                if guard.duel_queue.len() >= 2 && data.num_of_active_players + 2 < Settings::MaxPlayerNum as u16{
-                    handle_queue(&mut data, &mut guard.duel_queue, LobbyType::Duel);}
-                else if guard.group_queueu.len() >= 4 && data.num_of_active_players + 4 < Settings::MaxPlayerNum as u16{
-                    handle_queue(&mut data, &mut guard.group_queueu, LobbyType::Group);}
-                else if guard.batallion_queueu.len() >= 10 && data.num_of_active_players + 10 < Settings::MaxPlayerNum as u16{
-                    handle_queue(&mut data, &mut guard.batallion_queueu, LobbyType::Battalion);}
+            Ok(mut guarded_queues) => {
+                println!("Duel:{},Group{},Battalion{}",guarded_queues.duel_queue.len(),guarded_queues.group_queueu.len(),guarded_queues.batallion_queueu.len());
+
+                if guarded_queues.num_of_active_lobbies < Settings::MaxLobbyNum as u16 && guarded_queues.num_of_active_players + 2 < Settings::MaxPlayerNum as u16 && guarded_queues.duel_queue.len() >= 2 {
+                    if handle_queue(data.clone(), &mut guarded_queues.duel_queue, LobbyType::Duel){
+                        guarded_queues.num_of_active_players += 2;
+                        guarded_queues.num_of_active_lobbies += 1;
+                    }
+                }
+                else if guarded_queues.num_of_active_lobbies < Settings::MaxLobbyNum as u16 && guarded_queues.num_of_active_players + 4 < Settings::MaxPlayerNum as u16 && guarded_queues.group_queueu.len() >= 4{
+                    if handle_queue(data.clone(), &mut guarded_queues.group_queueu, LobbyType::Group){
+                        guarded_queues.num_of_active_players += 4;
+                        guarded_queues.num_of_active_lobbies += 1;
+                    }
+                }
+                else if guarded_queues.num_of_active_lobbies < Settings::MaxLobbyNum as u16 && guarded_queues.batallion_queueu.len() >= 10 && guarded_queues.num_of_active_players + 10 < Settings::MaxPlayerNum as u16{
+                    if handle_queue(data.clone(), &mut guarded_queues.batallion_queueu, LobbyType::Battalion){
+                        guarded_queues.num_of_active_players += 10;
+                        guarded_queues.num_of_active_lobbies += 1;
+                    }
+                }
             },
             Err(_)=>{}
         }
@@ -203,7 +219,27 @@ fn connections_function(){
         if elapsed_frame < tic_time {
             std::thread::sleep(tic_time - elapsed_frame);
         }
+        last = Instant::now();
     }
+}
+
+fn handle_queue(data : Arc<Mutex<Data>>, queue : &mut VecDeque<[char;10]>, lobbyType : LobbyType) -> bool{
+    match data.lock(){
+        Ok (mut guarded_data) => {
+            if guarded_data.free_lobbies.len() == 0 {return false}
+
+            let lobby_index : usize = guarded_data.free_lobbies.pop_front().unwrap();
+            for i in 0..lobbyType as usize{
+                println!("i{}",i);
+                let username = queue.pop_front().unwrap();
+                guarded_data.lobbies[lobby_index].players.push(username);
+            }
+            guarded_data.lobbies[lobby_index].lobby_type = lobbyType;
+            println!("{:?}", guarded_data.lobbies[lobby_index]);
+            true
+        },
+        Err (_) => {false}
+    }    
 }
 
 fn lauch(){
@@ -211,19 +247,33 @@ fn lauch(){
         Arc::new(Mutex::new(QueuesSys {
             duel_queue: VecDeque::<[char;10]>::new(),
             group_queueu:VecDeque::<[char;10]>::new(),
-            batallion_queueu: VecDeque::<[char;10]>::new()}));
-    //let queue = Arc::new(Mutex::new(VecDeque::<[char;10]>::new()));
+            batallion_queueu: VecDeque::<[char;10]>::new(),
+            num_of_active_lobbies : 0,
+            num_of_active_players : 0,
+        }
+    ));
+
+    let data : Arc<Mutex<Data>> = 
+        Arc::new(Mutex::new(Data{
+            lobbies : array_init(|_| Lobby::default()),
+            free_lobbies : (0..Settings::MaxLobbyNum as usize).collect(),
+            number_of_active_lobbies : 0,
+        } 
+    ));
+
     let cloned_queue1 = queues.clone();
     let cloned_queue2 = queues.clone();
-    let warp_handle 
-    = thread::spawn(move || tokio::runtime::Runtime::new().unwrap().block_on(warp_function(cloned_queue1)));
-    let queue_handle = thread::spawn( move || check_queue(cloned_queue2));
-    let reqwest_handle = thread::spawn (move || )
+    let cloned_data1 = data.clone();
+    let cloned_data2 = data.clone();
+
+    let warp_handle = thread::spawn(move || tokio::runtime::Runtime::new().unwrap().block_on(warp_manager(cloned_queue1)));
+    let queue_handle = thread::spawn( move || queues_manager(cloned_data1, cloned_queue2));
+    let logic_handle = thread::spawn (move || logic_manager(cloned_data2));
+    /*have a cli to control the app through the main thread, make it close correctly, hadle signals like control-C etc */
     warp_handle.join().unwrap();
     queue_handle.join().unwrap();
+    logic_handle.join().unwrap();
 }
-
-
 
 fn main()  {
     lauch();
